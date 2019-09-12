0e3a14acd6e554c7f350af50177759eb82a853c5
qiuch <qiu.ch@qq.com>
2019-09-12 14:22:07 +0800

    #

--- src/main/java/com/qch/shop/config/WebSecurityConfig.java
+++ src/main/java/com/qch/shop/config/WebSecurityConfig.java
@@ -1,6 +1,11 @@
 package com.qch.shop.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qch.shop.entity.Result;
+import com.qch.shop.filter.CustomAuthenticationFilter;
 import com.qch.shop.service.UserInfoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,8 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -30,35 +38,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/test/**").permitAll()
-                .and().formLogin().loginProcessingUrl("/user/login").defaultSuccessUrl("/user").successHandler(new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                httpServletResponse.setContentType("application/json;charset=utf-8");
-                PrintWriter out = httpServletResponse.getWriter();
-                out.write("{\"status\":\"ok\",\"msg\":\"登录成功\"}");
-                out.flush();
-                out.close();
-            }
-        })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                        httpServletResponse.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = httpServletResponse.getWriter();
-                        out.write("{\"status\":\"error\",\"msg\":\"登录失败\"}");
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .and()
-                .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
-                .and().csrf().disable();
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder builder) throws Exception{
-        builder.userDetailsService(userDetailsService).passwordEncoder(new NoPasswordEncode());
+                .antMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout().logoutUrl("user/logout")
+               .and().csrf().disable();
+
+        httpSecurity.addFilterAt(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setHideUserNotFoundExceptions(false);
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new NoPasswordEncode());
+        return provider;
+    }
+    @Bean
+    CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler((req, resp, authentication) -> {
+            resp.setContentType("application/json;charset=utf-8");
+            PrintWriter out = resp.getWriter();
+            Result result = Result.ok("登录成功!");
+            out.write(new ObjectMapper().writeValueAsString(result));
+            out.flush();
+            out.close();
+        });
+        filter.setAuthenticationFailureHandler((req, resp, e) -> {
+            resp.setContentType("application/json;charset=utf-8");
+            PrintWriter out = resp.getWriter();
+            Result respBean = Result.error(e instanceof UsernameNotFoundException ? "用户不存在" : "用户名或密码错误");
+            out.write(new ObjectMapper().writeValueAsString(respBean));
+            out.flush();
+            out.close();
+        });
+        filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/user/login", "POST"));
+        filter.setAuthenticationManager(authenticationManagerBean());
+        return filter;
+    }
 }
